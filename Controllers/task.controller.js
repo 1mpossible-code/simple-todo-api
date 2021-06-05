6d340089a4c41fa6e2353e7ac6bb09f009a551cf
Max <linme00p@gmail.com>
2021-06-05 18:53:42 +0300

    Add basic logic to task controller. Add logic to get all tasks and create test one in task.controller.js

--- Controllers/task.controller.js
+++ Controllers/task.controller.js
@@ -1,5 +1,23 @@
-const helloWorld = (req, res) => {
-    res.status(200).json({message: 'Hello World1!'});
+const Task = require('../Models/task')
+const mongoose = require('mongoose')
+
+// Get all tasks
+const getAllTasks = (req, res) => {
+    // Find all tasks
+    Task.find().then(
+        (tasks) => res.status(200).json(tasks)
+    );
+};
+
+// Create test task
+const createTestTask = (req, res) => {
+    // New task from Task model
+    const task = new Task({_id: mongoose.Types.ObjectId(), body: 'test one', completed: false});
+    // Save new task
+    task.save((err, task) => {
+        if (err) res.status(500).send('Internal server error');
+        res.status(200).send(`Created new test task with id: ${task.id}`);
+    })
 }
 
-module.exports = {helloWorld};
+module.exports = {getAllTasks, createTestTask};
