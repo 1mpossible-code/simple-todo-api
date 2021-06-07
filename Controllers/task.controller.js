f187e1caee27e2c7addbf4859a7eca998ff3aba5
Max <linme00p@gmail.com>
2021-06-07 15:06:24 +0300

    Update task.controller.js Add method to show one task by id. Refactor 'store' method

--- Controllers/task.controller.js
+++ Controllers/task.controller.js
@@ -2,29 +2,38 @@ const Task = require('../Models/task')
 const mongoose = require('mongoose')
 
 // Get all tasks
-const index = (req, res) => {
+const index = async (req, res) => {
     // Find all tasks
     Task.find().then(
         (tasks) => res.json(tasks),
     );
 };
 
+// Show specified task with the given :id
+const show = async (req, res) => {
+    const id = req.params.id;
+    // res.send(id)
+    const task = await Task.findById(id).exec();
+    res.json(task);
+}
+
 // Store new task
-const store = (req, res) => {
+const store = async (req, res) => {
     const body = req.body.body;
     const completed = req.body.completed || false;
     // New task from Task model
-    const task = new Task({
-        _id: mongoose.Types.ObjectId(),
-        body,
-        completed,
-    });
-    // Save new task
-    task.save((err, task) => {
-        // If error - send error
-        if (err) res.status(500).send('Internal server error');
-        res.json(task);
-    })
+    Task.create({
+            _id: mongoose.Types.ObjectId(),
+            body,
+            completed,
+        },
+        (err, task) => {
+            // If error - send error
+            if (err) res.status(500).send('Internal server error');
+            // Return created task
+            res.json(task);
+        }
+    );
 };
 
-module.exports = {index, store};
+module.exports = {index, show, store};
--- Routes/api.routes.js
+++ Routes/api.routes.js
@@ -3,7 +3,12 @@ const taskController = require('../Controllers/task.controller')
 
 const router = express.Router();
 
+// Get all tasks
 router.get('/', taskController.index);
+// Get task with specified :id
+router.get('/:id', taskController.show);
+// Store new task with POST request
+// Task MUST have 'body' and OPTIONAL 'completed'
 router.post('/store', taskController.store);
 
 module.exports = router;
